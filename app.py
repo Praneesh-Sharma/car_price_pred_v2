ad2e3a1fb7e86f189f6d69889594eca4cfe3f960
Praneesh Sharma <92104015+Praneesh-Sharma@users.noreply.github.com>
2024-03-16 15:29:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,29 @@ import pandas as pd
 import pickle
 
 st.title('Car Price Prediction')
+
+with open("car_price.sav", 'rb') as f:
+   wine_pred_model = pickle.load(f)
+
+col1, col2 = st.columns(2)
+
+with col1:
+  a = st.number_input("Present Price:",
+                      min_value=0.20,
+                      max_value=100.00,
+                      step=1
+      )
+
+with col2:
+  b = st.number_input("KMs driven",
+                      min_value=50,
+                      max_value=500000,
+                      step=1
+      )
+
+with col1:
+  c = st.selectbox("Fuel Type",
+                   ("Petrol", "Diesel", "CNG),
+                   index=None,
+                   placeholder="Select fuel type...",
+                        )
